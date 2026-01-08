407ddca9cb1f29901573f9a18cbb756850b1efe7
Rahulgaire <gairerahul334@gmail.com>
2026-01-08 20:56:42 +0530

    final render config

--- Hello/settings.py
+++ Hello/settings.py
@@ -41,8 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'hello',
-    'home'
+    'home',
     'whitenoise.runserver_nostatic',
 ]
 
@@ -55,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'home.apps.HomeConfig',
 ]
 
 ROOT_URLCONF = 'Hello.urls'
